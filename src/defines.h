d7e1936d1c5ce4c98c6814d97768c0c19e517665
facetoe <facetoe@ymail.com>
2013-02-23 15:16:06 +0800

    Forgot to add defines.h.

---
+++ src/defines.h
@@ -0,0 +1,23 @@
+/*
+ * defines.h
+ *
+ *  Created on: Feb 23, 2013
+ *      Author: fragmachine
+ */
+
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+#define MAX_TITLE_SIZE 200
+#define MAX_MESSAGE_SIZE 1000
+#define TIME_SIZE 30
+#define MAX_PATH_SIZE 200
+#define DEBUG 1
+
+/* Flushes junk characters in the input buffer */
+#define FLUSH_STDIN(x) {if(x[strlen(x)-1]!='\n'){do fgets(Junk,16,stdin);while(Junk[strlen(Junk)-1]!='\n');}else x[strlen(x)-1]='\0';}
+char Junk[16];
+
+char inputBuffer[MAX_MESSAGE_SIZE];
+
+#endif /* DEFINES_H_ */
