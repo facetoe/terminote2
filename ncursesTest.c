aeb31a8c68b957f8b0fbb678f5d2b0c9c45a05d4
facetoe <facetoe@ymail.com>
2013-04-14 10:30:51 +0800

    Got the header resizing properly

--- ncursesTest.c
+++ ncursesTest.c
@@ -19,7 +19,7 @@ typedef struct topWin {
 }_topWin;
 
 int x, y;
-WINDOW *topWin;
+WINDOW *topWin, *midWin;
 _topWin *winStruct;
 
 void init_topWin(_topWin **win) {
@@ -36,19 +36,50 @@ void init_topWin(_topWin **win) {
 
 void draw() {
 	endwin();
-	wclear(topWin);
+	if (topWin)
+		delwin(topWin);
+
 	char str[100];
 	int xt, yt;
+	char *sprintfKey = "/home/fragmachine/people/lol/hahahga";
+	int len = strlen(sprintfKey);
 	getmaxyx(stdscr, yt, xt);
+	topWin = newwin(1, xt, 0, 0);
+
+	midWin = newwin(yt, xt, 2, 2);
+	wrefresh(midWin);
+
+	start_color();
+	init_pair(1, COLOR_BLACK, COLOR_WHITE);
+	wattron(topWin,COLOR_PAIR(1));
+	wbkgd(topWin,COLOR_PAIR(1));
+
 	sprintf(str, "Coords: x: %d y: %d", xt, yt);
-	wborder(topWin, ' ', ' ', '-', '-', '+', '+', '+', '+');
-	mvwprintw(topWin, 1, (xt/2)-1, str);
+	mvwprintw(topWin, 0, (xt/2)-(len/2), sprintfKey);
+	mvwprintw(topWin, 0, 0, "Note #2");
+	char *time = "03/12/23 23:10";
+	len = strlen(time);
+	mvwprintw(topWin, 0, xt-(len+1), "03/12/23 23:10");
 	wrefresh(topWin);
 }
 
-static void hdl (int sig)
+static void hndlSigwinch (int sig)
 {
 	draw();
+}
+
+void initNcurses() {
+	initscr();
+	raw();
+	nonl();
+	noecho();
+	cbreak();
+	keypad(stdscr, TRUE);
+}
+
+void setTop(_topWin *win) {
+	win->noteNum = 1;
+	win->numLines = 4;
 }
 
 
@@ -57,25 +88,31 @@ int main (int argc, char *argv[])
 {
 	struct sigaction sa;
 	memset (&sa, '\0', sizeof(sa));
-	sa.sa_handler = hdl;
+	sa.sa_handler = hndlSigwinch;
 
-	initscr();
-	raw();
-	nonl();
-	noecho();
-	cbreak();
-	keypad(stdscr, TRUE);
 
+	initNcurses();
 
 	char str[100];
-	char *sprintfKey = "Coords: x: %d y: %d";
+	char *sprintfKey = "/home/facetoe/git/workspace/asss/lol";
 
 	getmaxyx(stdscr, y, x);
-	topWin = newwin(4, x, 0, 0);
+	topWin = newwin(1, x, 0, 0);
+
+	start_color();
+	init_pair(1, COLOR_BLACK, COLOR_WHITE);
+	wattron(topWin,COLOR_PAIR(1));
+	wbkgd(topWin,COLOR_PAIR(1));
+
+
+	midWin = newwin(y, x, 0, 0);
+	wrefresh(midWin);
+
 	int len = strlen(sprintfKey);
 	sprintf(str, "Coords: x: %d y: %d", x, y);
-	mvwprintw(topWin, 1, (x/2)-(len/2), str);
-	wborder(topWin, ' ', ' ', '-', '-', '+', '+', '+', '+');
+	mvwprintw(topWin, 0, (x/2)-(len/2), sprintfKey);
+	mvwprintw(topWin, 0, 0, "Note #2");
+	mvwprintw(topWin, 0, x-10, "Hello");
 	wrefresh(topWin);
 
 	sigemptyset(&sa.sa_mask);
