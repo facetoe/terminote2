a4b0b0d1dfd445a8c07da58e0a35732cec693fe0
facetoe <facetoe@ymail.com>
2013-05-13 11:17:28 +0800

    Getting ready to merge back into dev.

--- src/linkedList.h
+++ src/linkedList.h
@@ -15,57 +15,13 @@
 
 #include "defines.h"
 #include "helperFunctions.h"
+#include "structures.h"
 
 #include <unistd.h> // getcwd
 
 
 extern char * path;
 
-#define MAX_TIME_SIZE 30
-#define MAX_PATH_SIZE 200
-
-/* List to hold lines */
-struct line {
-    int lNum;
-    int lSize;
-    char *currLine;
-    struct line *next;
-    struct line *prev;
-};
-
-typedef struct line LINE;
-
-/* List to hold parsed messages */
-struct message {
-
-    int totalMessages;
-
-    int numLines;
-    int messageNum;
-    long numChars;
-
-    char path[MAX_PATH_SIZE];
-    char time[MAX_TIME_SIZE];
-
-    /* Top line of the message */
-    LINE *first;
-
-    /* Bottom line of the message */
-    LINE *last;
-
-    /* Top line of the current page */
-    LINE *pageTop;
-
-    /* Bottom line of the current page */
-    LINE *pageBot;
-
-    struct message *next;
-    struct message *prev;
-    struct message *root;
-
-};
-
-typedef struct message MESSAGE;
 
 
 /* Allocates memory for a new LINE node and sets default values */
--- src/terminote.c
+++ src/terminote.c
@@ -21,125 +21,6 @@ char *path = "/home/facetoe/.terminote.data";
 
 OPTIONS *opts = NULL;
 
-#define LINE_FIRST(msg) msg->first
-#define LINE_LAST(msg) msg->last
-
-LINE *line_getLineNode( MESSAGE *msg, int nodeNum ) {
-
-    if ( !msg ) {
-        fprintf( stderr, "Null pointer passed to line_getLineNode\n" );
-        exit( 1 );
-    }
-    LINE *tmp = msg->first;
-    if ( !tmp ) {
-        fprintf( stderr, "Invalid LINE pointer passed to line_getLineNode\n" );
-        exit( 1 );
-    } else if ( nodeNum > msg->numLines ) {
-        fprintf( stderr, "Invalid nodeNum passed to line_getLineNode\n" );
-        return NULL;
-    }
-
-    for ( ; tmp->next && tmp->lNum < nodeNum; tmp = tmp->next ) {
-        ;
-    }
-    return tmp;
-}
-
-void line_insertAfter( MESSAGE *msg, int nodeNum, char *str ) {
-
-    LINE *oldLine = NULL;
-
-    if ( ( oldLine = line_getLineNode( msg, nodeNum ) ) == NULL ) {
-        fprintf( stderr, "Unable to retrieve nodeNum %d\n", nodeNum );
-        return;
-    } else {
-
-        LINE *newLine = line_getLine();
-
-        /* Add the new line */
-        int strLen = strlen( str );
-        newLine->currLine = malloc( strLen );
-        memmove( newLine->currLine, str, strLen );
-        newLine->currLine[strLen] = '\0';
-
-        /* Update pointers */
-        newLine->next = oldLine->next;
-        oldLine->next = newLine;
-        newLine->prev = oldLine;
-        newLine->next->prev = newLine;
-
-        /* Update statistics */
-        newLine->lSize = strLen;
-        msg->numChars += newLine->lSize + 1;
-        msg->numLines++;
-    }
-}
-
-void line_insertBefore( MESSAGE *msg, int nodeNum, char *str ) {
-    LINE *oldLine, *newLine;
-    oldLine = NULL;
-    if ( ( oldLine = line_getLineNode( msg, nodeNum ) ) == NULL ) {
-        fprintf( stderr, "Unable to retrieve nodeNum %d\n", nodeNum );
-        return;
-    } else {
-        newLine = line_getLine();
-
-        /* Add the new line */
-        int strLen = strlen( str );
-        newLine->currLine = malloc( strLen );
-        memmove( newLine->currLine, str, strLen );
-        newLine->currLine[strLen] = '\0';
-
-        /* Update pointers */
-        newLine->prev = oldLine->prev;
-        newLine->next = oldLine;
-
-        /* if oldLine->prev we are inserting at the beginning */
-        if ( !oldLine->prev ) {
-            msg->first = newLine;
-        } else {
-            /* Otherwise insert as normal */
-            oldLine->prev->next = newLine;
-            oldLine->prev = newLine;
-        }
-
-        /* Update statistics */
-        newLine->lSize = strLen;
-        msg->numChars += newLine->lSize + 1;
-        msg->numLines++;
-    }
-}
-
-void line_deleteNode( MESSAGE *msg, int nodeNum ) {
-    LINE *nodeToBeDeleted = NULL;
-
-    if ( ( nodeToBeDeleted = line_getLineNode( msg, nodeNum ) ) == NULL ) {
-        fprintf(stderr, "Unable to delete nodeNum %d\n", nodeNum);
-        return;
-    } else {
-
-        if( !nodeToBeDeleted->prev ) {
-            msg->first = nodeToBeDeleted->next;
-        } else {
-            nodeToBeDeleted->prev->next = nodeToBeDeleted->next;
-        }
-
-        if( !nodeToBeDeleted->next ) {
-            msg->last = nodeToBeDeleted->prev;
-        } else {
-            nodeToBeDeleted->next->prev = nodeToBeDeleted->prev;
-        }
-    }
-
-    /* Update statistics */
-    msg->numLines--;
-    msg->numChars -= nodeToBeDeleted->lSize + 1;
-
-    /* Free the memory */
-    free(nodeToBeDeleted->currLine);
-    free(nodeToBeDeleted);
-}
-
 int main( int argc, char **argv ) {
 
     MESSAGE *msg = NULL;
@@ -148,9 +29,10 @@ int main( int argc, char **argv ) {
 
     msg = msg->next;
 
+
+    //list_deleteAll(&msg);
     //line_insertBefore( msg, 1, "I am inserted before 1" );
-    //line_insertAfter( msg, 0, "I am inserted after 2" );
-    line_deleteNode(msg, 1);
+    line_insertAfter( msg, 0, "I am inserted after 2" );
     list_printAll( stdout, msg );
 
     list_save( msg );
